37709ad457c8043e8920226647f051f4e5f5738f
Francesco <francesco.calzolai@gmail.com>
2020-04-17 19:13:45 +0100

    Add more tests

--- src/test/java/org/validator/ValidatorTest.java
+++ src/test/java/org/validator/ValidatorTest.java
@@ -1,8 +1,8 @@
 package org.validator;
 
 import io.vavr.collection.Seq;
-import io.vavr.control.Try;
 import io.vavr.control.Validation;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,31 +11,85 @@ import java.util.function.Function;
 
 import static io.vavr.control.Validation.invalid;
 import static io.vavr.control.Validation.valid;
+import static org.junit.Assert.fail;
 
 public class ValidatorTest {
 
   private static final List<Integer> items = List.of(1, 2, 3, 4);
 
-  private Validator<Integer, String, Boolean> bv1;
-  private Validator<Integer, String, Boolean> bv2;
+  private Validator<Integer, String, Boolean> booleanValidator1;
+  private Validator<Integer, String, Boolean> booleanValidator2;
 
   @Before
   public void before() {
-    bv1 = new Validator<>(items, isGreaterThan(0));
-    bv2 = new Validator<>(items, isGreaterThan(3));
+    booleanValidator1 = new Validator<>(items, isGreaterThan(0));
+    booleanValidator2 = new Validator<>(items, isGreaterThan(3));
   }
 
-  @Test
+//  @Test
   public void getValidationsError() {
-    Try<Seq<Boolean>> seqs = bv2.getValidations().toTry();
-    System.out.println(seqs);
+    Validation<Seq<String>, Seq<Boolean>> validation2 = booleanValidator2.getValidations();
+    Assert.assertEquals(3, validation2.getError().size());
    }
 
   @Test
   public void getValidations() {
-    Validation<Seq<String>, Seq<Boolean>> validation1 = bv1.getValidations();
-    Validation<Seq<String>, Seq<Boolean>> validation2 = bv2.getValidations();
-    System.out.println(validation1);;
+    Validation<Seq<String>, Seq<Boolean>> validation1 = booleanValidator1.getValidations();
+    Assert.assertEquals(items.size(), validation1.get().size());
+
+    Validation<Seq<String>, Seq<Boolean>> validation2 = booleanValidator2.getValidations();
+    Assert.assertEquals(3, validation2.getError().size());
+  }
+
+  @Test
+  public void getOrElseThrow() {
+    Seq<Boolean> res = null;
+    try {
+      res = booleanValidator1.getOrElseThrow((errors) -> new Exception());
+    } catch (Throwable throwable) {
+      fail(throwable.getMessage());
+    }
+    Assert.assertEquals(items.size(), res.size());
+
+    try {
+      booleanValidator1.getOrElseThrow((errors) -> new Exception());
+      fail("Should have thrown an exception");
+    } catch (Throwable throwable) {
+    }
+  }
+
+  @Test
+  public void getOrElseThrow2() {
+    Seq<Boolean> res = null;
+    try {
+      res = booleanValidator1.getOrElseThrow2(Exception::new);
+    } catch (Throwable throwable) {
+      fail(throwable.getMessage());
+    }
+    Assert.assertEquals(items.size(), res.size());
+
+    try {
+      booleanValidator1.getOrElseThrow2(Exception::new);
+      fail("Should have thrown an exception");
+    } catch (Throwable throwable) {
+    }
+  }
+
+  @Test
+  public void testGetOrElseThrow() {
+    Seq<Boolean> res = null;
+    try {
+      res = booleanValidator1.getOrElseThrow(Exception.class);
+    } catch (Throwable throwable) {
+      fail(throwable.getMessage());
+    }
+    Assert.assertEquals(items.size(), res.size());
+
+    try {
+      booleanValidator1.getOrElseThrow(Exception.class);
+      fail("Should have thrown an exception");
+    } catch (Throwable throwable) {
+    }
   }
 
   private static Function<Integer, Validation<String, Boolean>> isGreaterThan(int greaterThan) {
